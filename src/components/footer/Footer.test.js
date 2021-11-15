14af747d57260e178cc7083f90eec2e903a5eb13
Jay Witcher <jaewhicha@gmail.com>
2021-11-14 22:52:08 -0600

    fixed more tests

--- src/components/footer/Footer.test.js
+++ src/components/footer/Footer.test.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 
-import Footer from './Footer'
+import { Footer } from './Footer'
 
 describe('<Footer />', () => {
     const defaultProps = {}
